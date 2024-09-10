18af685f2c77d9bd2b4f658f25efb4acafa4124c
AliHermessi <alihermessi@gmail.com>
2024-09-10 23:06:03 +0100

    black line with tags added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { BlackLineComponent } from './black-line/black-line.component';
+import { ServicesCardsComponent } from './services-cards/services-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     IntroductionComponent,
-    BlackLineComponent
+    BlackLineComponent,
+    ServicesCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services-cards/services-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-cards',
+  templateUrl: './services-cards.component.html',
+  styleUrl: './services-cards.component.css'
+})
+export class ServicesCardsComponent {
+
+}
